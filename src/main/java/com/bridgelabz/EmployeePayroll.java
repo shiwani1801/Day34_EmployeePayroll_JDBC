7ddf75590bcc00acf294adebe051fab260f9ff34
shiwani <shiwanipaunikar1@gmail.com>
2023-02-22 21:58:53 -0800

    Added:Ability to find sum ,min,max,count and average of female and male employee

--- src/main/java/com/bridgelabz/EmployeePayroll.java
+++ src/main/java/com/bridgelabz/EmployeePayroll.java
@@ -10,6 +10,8 @@ public class EmployeePayroll {
         updateData(con);
         reteriveDataByName(con);
         particularDateRange(con);
+        operationOnGroupFemale(con);
+        operationOnGroupMale(con);
     }
     public static Connection connected() throws EmployeeCustomException {
         String jdbcURL = "jdbc:mysql://localhost:3306/payroll_service?useSSL=false"; // declare JdbcURL
@@ -90,7 +92,102 @@ public class EmployeePayroll {
                     "id:- " + id + " | "+" Name:- " + name + " | "+" Gender:- " + gender+" | "+" Salary:- " + salary + " | " + " Date:- " + date);
         }
     }
+    public static void operationOnGroupFemale(Connection connection) throws SQLException {
+        String salary = null;
+        String query = "select sum(salary) as salary from employee_payroll where gender = 'Female' group by gender";
+        PreparedStatement preparedStatement = con.prepareStatement(query);
+        ResultSet resultSet = preparedStatement.executeQuery();
+        while (resultSet.next()) {
+            salary = (resultSet.getString("salary"));
+            System.out.println("Sum of salary by gender(Female) is: "+salary);
 
+        }
+        String salary1 = null;
+        String query1 = "select  avg(salary) as salary1 from employee_payroll where gender = 'FeMale' group by gender";
+        PreparedStatement preparedStatement1 = con.prepareStatement(query1);
+        ResultSet resultSet1 = preparedStatement1.executeQuery();
+        while (resultSet1.next()) {
+            salary1 = (resultSet1.getString("salary1"));
+            System.out.println("avg of salary by gender(Female) is: "+salary1);
+
+        }
+        String salary2 = null;
+        String query2 = "select  min(salary) as salary2 from employee_payroll where gender = 'FeMale' group by gender";
+        PreparedStatement preparedStatement2 = con.prepareStatement(query2);
+        ResultSet resultSet2 = preparedStatement2.executeQuery();
+        while (resultSet2.next()) {
+            salary2 = (resultSet2.getString("salary2"));
+            System.out.println("min of salary by gender(Female) is: "+salary2);
+
+        }
+        String salary3 = null;
+        String query3 = "select  max(salary) as salary3 from employee_payroll where gender = 'FeMale' group by gender";
+        PreparedStatement preparedStatement3 = con.prepareStatement(query3);
+        ResultSet resultSet3 = preparedStatement3.executeQuery();
+        while (resultSet3.next()) {
+            salary3 = (resultSet3.getString("salary3"));
+            System.out.println("max of salary by gender(Female) is: "+salary3);
+
+        }
+        String salary4 = null;
+        String query4 = "select  count(salary) as salary4 from employee_payroll where gender = 'FeMale' group by gender";
+        PreparedStatement preparedStatement4 = con.prepareStatement(query4);
+        ResultSet resultSet4 = preparedStatement4.executeQuery();
+        while (resultSet4.next()) {
+            salary4 = (resultSet4.getString("salary4"));
+            System.out.println("count of salary by gender(Female) is: "+salary4);
+
+        }
+
+    }
+    public static void operationOnGroupMale(Connection connection) throws SQLException {
+        String salary = null;
+        String query = "select sum(salary) as salary from employee_payroll where gender = 'male' group by gender";
+        PreparedStatement preparedStatement = con.prepareStatement(query);
+        ResultSet resultSet = preparedStatement.executeQuery();
+        while (resultSet.next()) {
+            salary = (resultSet.getString("salary"));
+            System.out.println("Sum of salary by gender(male) is: "+salary);
+
+        }
+        String salary1 = null;
+        String query1 = "select  avg(salary) as salary1 from employee_payroll where gender = 'Male' group by gender";
+        PreparedStatement preparedStatement1 = con.prepareStatement(query1);
+        ResultSet resultSet1 = preparedStatement1.executeQuery();
+        while (resultSet1.next()) {
+            salary1 = (resultSet1.getString("salary1"));
+            System.out.println("avg of salary by gender(male) is: "+salary1);
+
+        }
+        String salary2 = null;
+        String query2 = "select  min(salary) as salary2 from employee_payroll where gender = 'Male' group by gender";
+        PreparedStatement preparedStatement2 = con.prepareStatement(query2);
+        ResultSet resultSet2 = preparedStatement2.executeQuery();
+        while (resultSet2.next()) {
+            salary2 = (resultSet2.getString("salary2"));
+            System.out.println("min of salary by gender(male) is: "+salary2);
+
+        }
+        String salary3 = null;
+        String query3 = "select  max(salary) as salary3 from employee_payroll where gender = 'Male' group by gender";
+        PreparedStatement preparedStatement3 = con.prepareStatement(query3);
+        ResultSet resultSet3 = preparedStatement3.executeQuery();
+        while (resultSet3.next()) {
+            salary3 = (resultSet3.getString("salary3"));
+            System.out.println("max of salary by gender(male) is: "+salary3);
+
+        }
+        String salary4 = null;
+        String query4 = "select  count(salary) as salary4 from employee_payroll where gender = 'Male' group by gender";
+        PreparedStatement preparedStatement4 = con.prepareStatement(query4);
+        ResultSet resultSet4 = preparedStatement4.executeQuery();
+        while (resultSet4.next()) {
+            salary4 = (resultSet4.getString("salary4"));
+            System.out.println("count of salary by gender(male) is: "+salary4);
+
+        }
+
+    }
     public static void listDrivers() {
         Enumeration<Driver> driverList = DriverManager.getDrivers();
         while (driverList.hasMoreElements()) { // static method for iteration.
